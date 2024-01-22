41e2b03e7f5b43126b857cd4f34fac9dcf5b8c22
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com>
2024-01-22 17:03:51 +0000

    Initial commit

---
+++ src/main/java/org/mindswap/academy/mindera_travel_agency/MinderaTravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package org.mindswap.academy.mindera_travel_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinderaTravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinderaTravelAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mindswap/academy/mindera_travel_agency/MinderaTravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mindswap.academy.mindera_travel_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinderaTravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
