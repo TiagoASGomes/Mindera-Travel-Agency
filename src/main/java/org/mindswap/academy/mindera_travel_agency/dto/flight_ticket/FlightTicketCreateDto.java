02d3234a4fa3efdc62918e6b56156f935ae8bb34
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com.>
2024-01-25 15:53:06 +0000

    FlightCreateDto

--- src/main/java/org/mindswap/academy/mindera_travel_agency/dto/flight_ticket/FlightTicketCreateDto.java
+++ src/main/java/org/mindswap/academy/mindera_travel_agency/dto/flight_ticket/FlightTicketCreateDto.java
@@ -1,6 +1,15 @@
 package org.mindswap.academy.mindera_travel_agency.dto.flight_ticket;
 
 public record FlightTicketCreateDto(
-        Long ticketNumber
+        Long ticketNumber,
+        String fName,
+        String email,
+        String phone,
+        String seatNumber,
+        int price,
+        String fareClass,
+        int maxLuggageWeight,
+        boolean carryOnLuggage,
+        Long invoiceId
 ) {
 }
