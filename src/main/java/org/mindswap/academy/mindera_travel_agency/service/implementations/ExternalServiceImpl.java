beb28148ba993f35fe692a2db82b45e27ab1eabe
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com.>
2024-02-07 21:02:29 +0000

    User tests

--- src/main/java/org/mindswap/academy/mindera_travel_agency/service/implementations/ExternalServiceImpl.java
+++ src/main/java/org/mindswap/academy/mindera_travel_agency/service/implementations/ExternalServiceImpl.java
@@ -91,7 +91,7 @@ public class ExternalServiceImpl implements ExternalService {
     public List<ExternalFlightInfoDto> getFlights(String source, String destination, String arrivalDate, int page) throws UnirestException, JsonProcessingException {
         objectMapper.registerModule(new JavaTimeModule());
         Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get(FLIGHT_API_URL + "/api/v1/flights/" + source + "?page=" + page)
+        HttpResponse<String> response = Unirest.get(FLIGHT_API_URL + "/api/v1/flights/" + source + "/" + destination + "/" + arrivalDate + "?page=" + page)
                 .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .asString();
         if (response.getStatus() == 200) {
